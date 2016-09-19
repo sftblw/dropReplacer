9de09fb13cd5a21398ee40b0a5d50adc199e20ea
ChanHwi, Choi (aka Ch.) <ccwpc@hanmail.net>
2016-09-19 22:28:08 +0900

    npm run build to external script to support multiple platform icon

---
+++ task/build.js
@@ -0,0 +1,17 @@
+'use strict';
+let packager = require('electron-packager');
+
+// win32
+packager({
+  dir: './',
+  arch: 'ia32,x64',
+  asar: true,
+  out: 'build',
+  platform: 'win32',
+  prune: true,
+  ignore: /(build|.gitignore|.gidmodules|res_bin)/,
+  icon: 'res_bin/dropReplacer_icon.ico'
+}, (err, appPaths) => {
+  if (err) console.error(err);
+  else console.info('npm run build:: Packaging done');
+});
