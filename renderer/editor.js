0d9e67129f003824d41954b77ffccb8d157ed9b2
ChanHwi, Choi (aka Ch.) <ccwpc@hanmail.net>
2016-11-28 23:18:26 +0900

    Replace initial value to original one
    wow → (.*), $1 (renamed)

--- renderer/editor.js
+++ renderer/editor.js
@@ -1,20 +1,20 @@
 'use strict';
+const codemirror = require('codemirror');
 
 module.exports = {
-    inputRegexEditor: null,
-    outputRegexEditor: null
+    inputRegex: null,
+    outputRegex: null
 };
-require("codemirror/mode/javascript/javascript");
-const codemirror = require('codemirror');
+
 window.addEventListener('load', (loadev) => {
     let inputRegexTextArea = document.querySelector('textarea.inputRegex');
     let outputRegexTextArea = document.querySelector('textarea.outputRegex');
-    module.exports.inputRegexEditor = codemirror.fromTextArea(inputRegexTextArea, {
-        value: "(.*)",
-        mode: "javascript"
+    module.exports.inputRegex = codemirror.fromTextArea(inputRegexTextArea, {
+        lineWrapping: true,
+        dragDrop: false,
     });
-    module.exports.outputRegexEditor = codemirror.fromTextArea(outputRegexTextArea, {
-        value: "$1 (renamed)",
-        mode: "javascript"
+    module.exports.outputRegex = codemirror.fromTextArea(outputRegexTextArea, {
+        lineWrapping: true,
+        dragDrop: false,
     });
 });
